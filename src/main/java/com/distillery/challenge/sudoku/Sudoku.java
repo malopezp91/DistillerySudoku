f651a97ffe6a566ab31ee6c7d8a16d5d41752cb1
Miguel Lopez <malopezp.91@gmail.com>
2020-07-11 15:57:00 -0500

    Implementation of all Sudoku validations

--- src/main/java/com/distillery/challenge/sudoku/Sudoku.java
+++ src/main/java/com/distillery/challenge/sudoku/Sudoku.java
@@ -1,12 +1,107 @@
 package com.distillery.challenge.sudoku;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
 /**
  * Sudoku
  * Contains methods that validates a Sudoku array
  */
 final class Sudoku {
 
+    private static final int BOARD_SIZE = 9;
+    private static final int BOX_SIZE = 3;
+
     public boolean isValidSudoku(char[][] board) {
-        return false;
+        return Objects.nonNull(board) &&
+                isValidSize(board) &&
+                areRowsValid(board) &&
+                areColumnsValid(board) &&
+                areBoxesValid(board);
+    }
+
+    private boolean isValidSize(char[][] board) {
+        // Number of rows must match BOARD_SIZE,
+        // and all rows must contain BOARD_SIZE number of items
+        if (board.length == BOARD_SIZE) {
+            for (char[] row : board) {
+                if (row.length != BOARD_SIZE) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean areRowsValid(char[][] board) {
+        // All rows must contain not repeating digits. Using a Set ot perform this validation easily
+        for (char[] row : board) {
+            Set<Character> helperSet = new HashSet<>();
+            for (char item : row) {
+                if (Character.isDigit(item)) {
+                    if (helperSet.contains(item)) {
+                        return false;
+                    } else {
+                        helperSet.add(item);
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean areColumnsValid(char[][] board) {
+        // All rows, at the same columnIndex, must contain not repeating digits.
+        // Using a Set ot perform this validation easily
+        for (int columnIndex = 0; columnIndex < BOARD_SIZE; columnIndex++) {
+            Set<Character> helperSet = new HashSet<>();
+            for (char[] row : board) {
+                char item = row[columnIndex];
+                if (Character.isDigit(item)) {
+                    if (helperSet.contains(item)) {
+                        return false;
+                    } else {
+                        helperSet.add(item);
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean areBoxesValid(char[][] board) {
+        // For each box, a Set will be used to validate if there are non repeating digits.
+        List<Set<Character>> helperList = new ArrayList<>();
+        for (int rowIndex = 0; rowIndex < BOARD_SIZE; rowIndex++) {
+
+            // When we hit a row that represents a box, Sets are rebooted
+            if (rowIndex % BOX_SIZE == 0) {
+                helperList.clear();
+                int numOfBoxes = BOARD_SIZE / BOX_SIZE;
+                for (int boxCounter = 0; boxCounter < numOfBoxes; boxCounter++) {
+                    helperList.add(new HashSet<>());
+                }
+            }
+
+            char[] row = board[rowIndex];
+            for (int columnIndex = 0; columnIndex < BOARD_SIZE; columnIndex++) {
+                int boxHelperSet = columnIndex / BOX_SIZE;
+                Set<Character> helperSet = helperList.get(boxHelperSet);
+                char item = row[columnIndex];
+                if (Character.isDigit(item)) {
+                    if (helperSet.contains(item)) {
+                        return false;
+                    } else {
+                        helperSet.add(item);
+                    }
+                }
+            }
+        }
+        return true;
     }
 }
--- src/test/java/com/distillery/challenge/sudoku/SudokuTest.java
+++ src/test/java/com/distillery/challenge/sudoku/SudokuTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * SudokuTest
@@ -19,7 +20,121 @@ final class SudokuTest {
     }
 
     @Test
-    public void isValid_sudokIsNull_returnsFalse() {
+    public void isValid_sudokuIsNull_returnsFalse() {
         assertFalse(sudoku.isValidSudoku(null));
+    }
+
+    @Test
+    public void isValid_sudokuHas8Rows_returnsFalse() {
+        char[][] input = new char[][]{
+                new char[]{'8', '3', '.', '.', '7', '.', '.', '.', '.'},
+                new char[]{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                new char[]{'.', '9', '7', '.', '.', '.', '.', '6', '.'},
+                new char[]{'1', '.', '.', '.', '6', '.', '.', '.', '3'},
+                new char[]{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                new char[]{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                new char[]{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                new char[]{'.', '.', '.', '4', '1', '9', '.', '.', '5'}};
+                // Only 8 rows
+
+        assertFalse(sudoku.isValidSudoku(input));
+    }
+
+    @Test
+    public void isValid_sudokuHas9RowsButOneWith8Elements_returnsFalse() {
+        char[][] input = new char[][]{
+                new char[]{'8', '3', '.', '.', '7', '.', '.', '.', '.'},
+                new char[]{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                new char[]{'.', '9', '7', '.', '.', '.', '.', '6', '.'},
+                new char[]{'1', '.', '.', '.', '6', '.', '.', '.', '3'},
+                new char[]{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                new char[]{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                new char[]{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                new char[]{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                new char[]{'.', '.', '.', '.', '8', '.', '.', '7'}}; // Invalid row, only 8 elements
+
+        assertFalse(sudoku.isValidSudoku(input));
+    }
+
+    @Test
+    public void isValid_sudokuHasRowWithRepeatedElements_returnsFalse() {
+        char[][] input = new char[][]{
+                new char[]{'8', '3', '.', '.', '7', '.', '.', '.', '8'}, // Invalid row, has two '8'
+                new char[]{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                new char[]{'.', '9', '7', '.', '.', '.', '.', '6', '.'},
+                new char[]{'1', '.', '.', '.', '6', '.', '.', '.', '3'},
+                new char[]{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                new char[]{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                new char[]{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                new char[]{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                new char[]{'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+
+        assertFalse(sudoku.isValidSudoku(input));
+    }
+
+    @Test
+    public void isValid_sudokuHasColumnWithRepeatedElements_returnsFalse() {
+        char[][] input = new char[][]{
+                // First column has two '3'
+                new char[]{'8', '3', '.', '.', '7', '.', '.', '.', '.'},
+                new char[]{'3', '.', '.', '1', '9', '5', '.', '.', '.'},
+                new char[]{'.', '9', '7', '.', '.', '.', '.', '6', '.'},
+                new char[]{'1', '.', '.', '.', '6', '.', '.', '.', '3'},
+                new char[]{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                new char[]{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                new char[]{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                new char[]{'3', '.', '.', '4', '1', '9', '.', '.', '5'},
+                new char[]{'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+
+        assertFalse(sudoku.isValidSudoku(input));
+    }
+
+    @Test
+    public void isValid_sudokuHasInvalidBox_returnsFalse() {
+        char[][] input = new char[][]{
+                // First Box has two '8'
+                new char[]{'8', '3', '.', '.', '7', '.', '.', '.', '.'},
+                new char[]{'3', '.', '.', '1', '9', '5', '.', '.', '.'},
+                new char[]{'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                new char[]{'1', '.', '.', '.', '6', '.', '.', '.', '3'},
+                new char[]{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                new char[]{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                new char[]{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                new char[]{'2', '.', '.', '4', '1', '9', '.', '.', '5'},
+                new char[]{'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+
+        assertFalse(sudoku.isValidSudoku(input));
+    }
+
+    @Test
+    public void isValid_sudokuIsValidWithEmptyElements_returnsTrue() {
+        char[][] input = new char[][]{
+                new char[]{'8', '3', '.', '.', '7', '.', '.', '.', '.'},
+                new char[]{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                new char[]{'.', '9', '7', '.', '.', '.', '.', '6', '.'},
+                new char[]{'1', '.', '.', '.', '6', '.', '.', '.', '3'},
+                new char[]{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                new char[]{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                new char[]{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                new char[]{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                new char[]{'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+
+        assertTrue(sudoku.isValidSudoku(input));
+    }
+
+    @Test
+    public void isValid_sudokuIsValidWithFullElements_returnsTrue() {
+        char[][] input = new char[][]{
+                new char[]{'1', '2', '3', '6', '7', '8', '9', '4', '5'},
+                new char[]{'5', '8', '4', '2', '3', '9', '7', '6', '1'},
+                new char[]{'9', '6', '7', '1', '4', '5', '3', '2', '8'},
+                new char[]{'3', '7', '2', '4', '6', '1', '5', '8', '9'},
+                new char[]{'6', '9', '1', '5', '8', '3', '2', '7', '4'},
+                new char[]{'4', '5', '8', '7', '9', '2', '6', '1', '3'},
+                new char[]{'8', '3', '6', '9', '2', '4', '1', '5', '7'},
+                new char[]{'2', '1', '9', '8', '5', '7', '4', '3', '6'},
+                new char[]{'7', '4', '5', '3', '1', '6', '8', '9', '2'}};
+
+        assertTrue(sudoku.isValidSudoku(input));
     }
 }
